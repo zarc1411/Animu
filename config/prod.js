632e3bce249e3fbf88efd0cab032470e3dc570db
Abdul Sammad <abdulsammad200@gmail.com>
2019-06-07 14:25:10 +0500

    Removed docker related files

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoConnectionString: process.env.MONGO_CONNECTION_STRING,
+  discordBotToken: process.env.DISCORD_BOT_TOKEN
+};
--- server/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  discordBotToken: process.env.DISCORD_BOT_TOKEN,
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
-  mongoConnectionString: process.env.MONGO_CONNECTION_STRING,
-  sessionSecret: process.env.SESSION_SECRET
-};
