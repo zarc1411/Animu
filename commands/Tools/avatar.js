6eb8198682db193bac84fa9ea36848f0a078a28b
Abdul Sammad <abdulsammad200@gmail.com>
2019-07-05 10:44:21 +0500

    Added avatar Command

---
+++ commands/Tools/avatar.js
@@ -0,0 +1,25 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Shows a user's avatar",
+      cooldown: 10,
+      extendedHelp:
+        "Mention a member to show their avatar or don't mention anyone to view your own avatar",
+      usage: '[user:user]'
+    });
+  }
+
+  async run(msg, [user = msg.author]) {
+    const avatar = user.displayAvatarURL({ size: 512 });
+
+    return msg.sendEmbed(
+      new MessageEmbed()
+        .setAuthor(user.username, avatar)
+        .setImage(avatar)
+        .setColor('#2196f3')
+    );
+  }
+};
