7bc8759a24802c8a96a00351d465c18bbc6ae33b
Abdul Sammad <abdulsammad200@gmail.com>
2019-07-06 10:02:48 +0500

    Added react Command

---
+++ commands/Fun/react.js
@@ -0,0 +1,42 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+//Init
+const Reaction = mongoose.model('Reaction');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text'],
+      aliases: ['r', 'reaction'],
+      cooldown: 5,
+      description: 'Express your reactions',
+      extendedHelp: 'Express different reactions',
+      usage: '<reactionName:string>',
+      usageDelim: ' '
+    });
+  }
+
+  async run(msg, [reactionName]) {
+    const reaction = await Reaction.findOne({
+      name: reactionName.toLowerCase()
+    }).exec();
+
+    if (!reaction)
+      return msg.sendEmbed(
+        new MessageEmbed()
+          .setTitle('Reaction not found')
+          .setDescription(`The reaction you're trying to use doesn't exist`)
+          .setColor('#f44336')
+      );
+
+    return msg.sendEmbed(
+      new MessageEmbed()
+        .setTitle(`${msg.member.displayName} is ${reactionName}`)
+        .setImage(_.sample(reaction.urls))
+        .setColor('#2196f3')
+    );
+  }
+};
--- commands/Profile/register.js
+++ commands/Profile/register.js
@@ -5,7 +5,6 @@ module.exports = class extends Command {
   constructor(...args) {
     super(...args, {
       runIn: ['text', 'dm', 'group'],
-      aliases: ['r'],
       cooldown: 60,
       description: 'Register profile',
       extendedHelp: 'Register your profile'
