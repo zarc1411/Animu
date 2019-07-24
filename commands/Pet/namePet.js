e64d4536005629e1c876901289df1bffeb3570f8
Light Yagami <abdulsammad200@gmail.com>
2019-07-24 17:52:05 +0500

    Added namePet Command

---
+++ commands/Pet/namePet.js
@@ -0,0 +1,43 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const mongoose = require('mongoose');
+
+//Init
+const Pet = mongoose.model('Pet');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'dm', 'group'],
+      aliases: ['changepetname', 'updatepetname'],
+      cooldown: 120,
+      description: 'Change name of your pet',
+      extendedHelp: 'Change name of your pet',
+      usage: '<name:...string{1,15}>',
+      quotedStringSupport: true
+    });
+  }
+
+  async run(msg, [name]) {
+    const pet = await Pet.findOne({ memberID: msg.author.id }).exec();
+
+    if (!pet)
+      return msg.sendEmbed(
+        new MessageEmbed()
+          .setTitle(`Oooops!`)
+          .setDescription("You don't own a pet")
+          .setColor('#f44336')
+      );
+
+    pet.petName = name;
+
+    await pet.save();
+
+    return msg.sendEmbed(
+      new MessageEmbed()
+        .setTitle(`Changed name`)
+        .setDescription(`Your pet's new name is ${name}`)
+        .setColor('#2196f3')
+    );
+  }
+};
