1a6c3337c9f0dd2c84469e42f256ad961f0a0a6c
Light Yagami <abdulsammad200@gmail.com>
2019-08-06 13:19:18 +0500

    Added owoify Command

---
+++ commands/Fun/owoify.js
@@ -0,0 +1,23 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text'],
+      aliases: ['owo', 'owofy'],
+      cooldown: 10,
+      description: 'Owoify your text',
+      usage: '<text:string>'
+    });
+  }
+
+  async run(msg, [text]) {
+    msg.sendEmbed(
+      new MessageEmbed()
+        .setTitle(`${msg.member.displayName} says`)
+        .setDescription(text.replace(/r|l/g, 'w').replace(/R|L/g, 'W'))
+        .setColor('#2196f3')
+    );
+  }
+};
