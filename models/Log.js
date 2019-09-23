74f97683b9ae5d07d4edadd9cd883b75ea71f63a
Light Yagami <abdulsammad200@gmail.com>
2019-09-23 20:26:05 +0500

    Updated Log Schema

--- models/Log.js
+++ models/Log.js
@@ -2,8 +2,12 @@ const { Schema, model } = require('mongoose');
 
 const logSchema = new Schema({
   guildID: String,
-  logType: String,
-  value: String,
+  event: String,
+  dateTime: {
+    type: Date,
+    default: Date.now,
+  },
+  data: Schema.Types.Mixed,
 });
 
 model('Log', logSchema);
