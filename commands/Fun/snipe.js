08c89b2da017c0e282c6b896eb5073376e25f9dc
Light Yagami <abdulsammad200@gmail.com>
2019-09-24 13:45:52 +0500

    Added snipe Command

---
+++ commands/Fun/snipe.js
@@ -0,0 +1,53 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+//Init
+const Log = mongoose.model('Log');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text'],
+      aliases: ['snipe', 'undelete'],
+      cooldown: 60,
+      description: 'Express your reactions',
+      extendedHelp: 'Express different reactions',
+    });
+  }
+
+  async run(msg) {
+    const deletedMsg = await Log.findOne(
+      {
+        guildID: msg.guild.id,
+        event: 'messageDelete',
+        'data.channelID': msg.channel.id,
+      },
+      {},
+      { sort: { created_at: -1 } },
+    );
+
+    if (!deletedMsg)
+      return msg.sendEmbed(
+        new MessageEmbed({
+          title: 'Oops!',
+          description: 'No message was recently deleted in this channel',
+          color: '#f44336',
+        }),
+      );
+
+    msg.sendEmbed(
+      new MessageEmbed({
+        description: deletedMsg.data.content,
+        author: {
+          name: this.client.users.get(deletedMsg.data.authorID).username,
+          iconURL: this.client.users
+            .get(deletedMsg.data.authorID)
+            .displayAvatarURL({ size: 64 }),
+        },
+        color: '#2196f3',
+      }),
+    );
+  }
+};
