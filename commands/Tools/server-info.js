d958b453f9006b8406f9f51f49577fb805ed7f79
Light Yagami <abdulsammad200@gmail.com>
2019-09-28 16:43:14 +0500

    Updated server Command

--- commands/Tools/server-info.js
+++ commands/Tools/server-info.js
@@ -1,6 +1,11 @@
 const { Command, Timestamp } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 const moment = require('moment');
+const { model } = require('mongoose');
+const _ = require('lodash');
+
+const Guild = model('Guild');
+const Key = model('Key');
 
 module.exports = class extends Command {
   constructor(...args) {
@@ -10,49 +15,62 @@ module.exports = class extends Command {
       description: 'Get information on the current server.',
       cooldown: 10,
       extendedHelp:
-        'Get information about this server, including creation date, filter/verification level, member count and other details'
+        'Get information about this server, including creation date, filter/verification level, member count and other details',
     });
     this.verificationLevels = [
       'None',
       'Low',
       'Medium',
       '(╯°□°）╯︵ ┻━┻',
-      '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻'
+      '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻',
     ];
 
     this.filterLevels = ['Off', 'No Role', 'Everyone'];
     this.timestamp = new Timestamp('d MMMM YYYY');
   }
 
-  run(msg) {
+  async run(msg) {
+    const guild = await Guild.findOne({ guildID: msg.guild.id }).exec();
+
+    if (!guild) return false;
+
+    const key = await Key.findOne({ key: guild.key }).exec();
+
     return msg.sendEmbed(
       new MessageEmbed()
-        .setColor(0x2196f3)
+        .setColor(
+          key.version === 'lite'
+            ? '#ffeb3b'
+            : key.version === 'plus'
+            ? '#f44336'
+            : '#9c27b0',
+        )
         .setThumbnail(msg.guild.iconURL())
         .addField('❯ Name', msg.guild.name, true)
         .addField('❯ ID', msg.guild.id, true)
         .addField(
           '❯ Creation Date',
           this.timestamp.display(msg.guild.createdAt),
-          true
+          true,
         )
         .addField('❯ Region', msg.guild.region, true)
         .addField(
           '❯ Explicit Filter',
           this.filterLevels[msg.guild.explicitContentFilter],
-          true
+          true,
         )
         .addField(
           '❯ Verification Level',
           this.verificationLevels[msg.guild.verificationLevel],
-          true
+          true,
         )
         .addField(
           '❯ Server Age',
           `${moment().diff(msg.guild.createdAt, 'M')} Months`,
-          true
+          true,
         )
         .addField('❯ Members', msg.guild.memberCount, true)
+        .setFooter(`Animu ${_.capitalize(key.version)}`),
     );
   }
 };
