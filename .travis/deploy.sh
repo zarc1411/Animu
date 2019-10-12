aa51a4be2691f55a11daccaa30d7d49bd6c114f5
Light Yagami <abdulsammad200@gmail.com>
2019-10-12 20:19:33 +0500

    Updated deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-set -e
-git config --global push.default simple
-git remote add production ssh://lightyagami@140.82.39.61/srv/Animu
-git push production master
+cd /srv/Animu
+git clone https://github.com/LightYagami200/Animu
+cd Animu
+pm2 start
