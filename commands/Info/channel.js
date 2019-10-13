3ec4b98c7c4b2f23e724976bc53290a76e9dbfa8
Light Yagami <abdulsammad200@gmail.com>
2019-10-13 13:16:39 +0500

    Added channel Command

---
+++ commands/Info/channel.js
@@ -0,0 +1,50 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text'],
+      aliases: ['channel-info', 'channelinfo', 'chinfo'],
+      requiredPermissions: ['EMBED_LINKS'],
+      description: 'Get information about a channel',
+      cooldown: 10,
+      usage: '<channel:channel>',
+    });
+    this.types = {
+      dm: 'DM',
+      group: 'Group DM',
+      text: 'Text Channel',
+      voice: 'Voice Channel',
+      category: 'Category',
+      unknown: 'Unknown',
+    };
+  }
+
+  async run(msg, [channel]) {
+    const embed = new MessageEmbed()
+      .setColor(0x2196f3)
+      .addField(
+        '❯ Name',
+        channel.type === 'dm' ? `@${channel.recipient.username}` : channel.name,
+        true,
+      )
+      .addField('❯ ID', channel.id, true)
+      .addField('❯ NSFW', channel.nsfw ? 'Yes' : 'No', true)
+      .addField(
+        '❯ Category',
+        channel.parent ? channel.parent.name : 'None',
+        true,
+      )
+      .addField('❯ Type', this.types[channel.type], true)
+      .addField(
+        '❯ Creation Date',
+        moment.utc(channel.createdAt).format('MM/DD/YYYY h:mm A'),
+        true,
+      )
+      .addField('❯ Topic', channel.topic || 'None');
+
+    return msg.send(embed);
+  }
+};
