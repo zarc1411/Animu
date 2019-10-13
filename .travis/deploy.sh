93ec79097bb8e3bd44d3296dc29c6ebb2a955173
Light Yagami <abdulsammad200@gmail.com>
2019-10-13 10:13:04 +0500

    Updated deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,9 @@ git remote add deploy ssh://lightyagami@140.82.39.61/srv/Animu
 git push deploy master
 
 ssh lightyagami@140.82.39.61 <<EOF
+  . /etc/profile
+  cp /srv/env/.env /srv/Animu/
   cd /srv/Animu
-  pm2 start
+  yarn
+  pm2 restart
 EOF
