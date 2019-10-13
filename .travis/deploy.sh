8b74d343589f31b57db60f067e60dbeaaabd8351
Light Yagami <abdulsammad200@gmail.com>
2019-10-13 10:21:59 +0500

    updated script again...

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,8 +5,7 @@ git remote add deploy ssh://lightyagami@140.82.39.61/srv/Animu
 git push deploy master
 
 ssh lightyagami@140.82.39.61 <<EOF
-  . /etc/profile
-  source ~/.bashrc
+  . ~/.profile
   cp /srv/env/.env /srv/Animu/
   cd /srv/Animu
   ls -a
