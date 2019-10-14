3edee6920a8f1862782f999a5eccd392a438c468
Light Yagami <abdulsammad200@gmail.com>
2019-10-14 11:51:57 +0500

    Added eggs-get-laid Command

---
+++ commands/Fun/eggs-get-laid.js
@@ -0,0 +1,21 @@
+const { Command } = require('klasa');
+const _ = require('lodash');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'dm', 'group'],
+      aliases: ['dark-light'],
+      requiredPermissions: ['EMBED_LINKS'],
+      cooldown: 10,
+      description: 'Eggs get laid',
+      usage: '[user:member]',
+    });
+  }
+
+  async run(msg, [user = msg.author]) {
+    msg.send(`${user}`, {
+      files: [`${__dirname}/../../images/eggs-get-laid.png`],
+    });
+  }
+};
