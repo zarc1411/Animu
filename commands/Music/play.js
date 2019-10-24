a2c74b01e205cebe3ffb75a26f57f2991d2dec48
Light Yagami <abdulsammad200@gmail.com>
2019-10-24 15:20:26 +0500

    Added playlist support

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -23,15 +23,13 @@ module.exports = class extends Command {
       cooldown: 5,
       description: 'Play Music',
       usage: '<music:...string>',
+      extendedHelp:
+        'Play music using either:\n- URL\n- Search Term\n- Playlist URL\n\nPlease note that playlist support is currently in BETA and only first 20 songs will be played from playlist',
       quotedStringSupport: true,
     });
   }
 
   async run(msg, [music]) {
-    const musicQueue = await MusicQueue.findOne({
-      guildID: msg.guild.id,
-    }).exec();
-
     const voiceChannel = msg.member.voice.channel;
 
     if (!voiceChannel)
@@ -49,94 +47,136 @@ module.exports = class extends Command {
         "It seems I don't have perms in Voice Channel that you're currently in",
       );
 
-    let video;
-    const cached = await redisClient.hgetAsync(
-      'yt_searches',
-      music.toLowerCase().trim(),
-    );
-    if (!cached) {
-      console.log('Using Search API');
-      try {
-        video = await youtube.getVideo(music);
-      } catch (error) {
+    if (
+      !music.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)
+    ) {
+      // ? Is NOT Playlist
+
+      let video;
+
+      const cached = await redisClient.hgetAsync(
+        'yt_searches',
+        music.toLowerCase().trim(),
+      );
+
+      if (!cached) {
+        // ? Using Search API
         try {
-          video = (await youtube.search(music, 1))[0];
-          await redisClient.hsetAsync(
-            'yt_searches',
-            music.toLowerCase().trim(),
-            video.url,
-          );
+          video = await youtube.getVideo(music);
         } catch (error) {
+          try {
+            video = (await youtube.search(music, 1))[0];
+            await redisClient.hsetAsync(
+              'yt_searches',
+              music.toLowerCase().trim(),
+              video.url,
+            );
+          } catch (error) {
+            return msg.send(
+              new MessageEmbed({
+                title: 'Music Not Found',
+                description:
+                  "The music you're trying to play wasn't found on YouTube",
+                color: '#f44336',
+              }),
+            );
+          }
+        }
+      } else {
+        // ? Using Cache
+        video = await youtube.getVideo(cached);
+      }
+      this.handleMusic(voiceChannel, video, msg);
+    } else {
+      // ? Is Playlist
+      const playlist = await youtube.getPlaylist(music);
+      const videos = await playlist.getVideos(20);
+      if (videos.length === 0)
+        return msg.send(
+          new MessageEmbed({
+            title: 'Empty Playlist',
+            description: "The playlist you're trying to play is empty",
+            color: '#f44336',
+          }),
+        );
+
+      for (const video of videos) {
+        await this.handleMusic(voiceChannel, video, msg);
+      }
+    }
+  }
+
+  handleMusic(voiceChannel, video, msg) {
+    // eslint-disable-next-line no-async-promise-executor
+    return new Promise(async (resolve) => {
+      const musicQueue = await MusicQueue.findOne({
+        guildID: msg.guild.id,
+      }).exec();
+
+      if (!video)
+        return msg.send(
+          new MessageEmbed({
+            title: 'Music Not Found',
+            description:
+              "The music you're trying to play wasn't found on YouTube",
+            color: '#f44336',
+          }),
+        );
+
+      if (!musicQueue) {
+        const queue = new MusicQueue({
+          guildID: msg.guild.id,
+          songs: [],
+          volume: msg.guild.settings.defaultVolume,
+        });
+
+        queue.songs.push({
+          title: Util.escapeMarkdown(video.title),
+          url: video.url,
+        });
+
+        await queue.save();
+
+        try {
+          const connection = await voiceChannel.join();
+          this.play(
+            msg.guild.id,
+            msg,
+            connection,
+            queue.songs[0],
+            queue.volume,
+          );
+        } catch (err) {
+          await MusicQueue.deleteOne({ guildID: msg.guild.id }).exec();
           return msg.send(
             new MessageEmbed({
-              title: 'Music Not Found',
-              description:
-                "The music you're trying to play wasn't found on YouTube",
+              title: "Can't Join",
+              description: 'There was an error while trying to join VC',
               color: '#f44336',
             }),
           );
         }
-      }
-    } else {
-      console.log('Using Cache');
-      video = await youtube.getVideo(cached);
-    }
+      } else {
+        musicQueue.songs.push({
+          title: Util.escapeMarkdown(video.title),
+          url: video.url,
+        });
 
-    if (!video)
-      return msg.send(
-        new MessageEmbed({
-          title: 'Music Not Found',
-          description:
-            "The music you're trying to play wasn't found on YouTube",
-          color: '#f44336',
-        }),
-      );
+        await musicQueue.save();
 
-    if (!musicQueue) {
-      const queue = new MusicQueue({
-        guildID: msg.guild.id,
-        songs: [],
-        volume: msg.guild.settings.defaultVolume,
-      });
-
-      queue.songs.push({
-        title: Util.escapeMarkdown(video.title),
-        url: video.url,
-      });
-
-      await queue.save();
-
-      try {
-        const connection = await voiceChannel.join();
-        this.play(msg.guild.id, msg, connection, queue.songs[0], queue.volume);
-      } catch (err) {
-        await MusicQueue.deleteOne({ guildID: msg.guild.id }).exec();
-        return msg.send(
+        msg.send(
           new MessageEmbed({
-            title: "Can't Join",
-            description: 'There was an error while trying to join VC',
-            color: '#f44336',
+            title: `Added to Queue`,
+            description: `**${Util.escapeMarkdown(
+              video.title,
+            )}** is added to queue`,
+            color: '#2196f3',
           }),
         );
       }
-    } else {
-      musicQueue.songs.push({
-        title: Util.escapeMarkdown(video.title),
-        url: video.url,
-      });
 
-      await musicQueue.save();
-
-      msg.send(
-        new MessageEmbed({
-          title: `Added to Queue`,
-          description: `**${Util.escapeMarkdown(
-            video.title,
-          )}** is added to queue`,
-          color: '#2196f3',
-        }),
-      );
-    }
+      resolve(true);
+    });
   }
 
   async play(guildID, msg, connection, song, volume) {
