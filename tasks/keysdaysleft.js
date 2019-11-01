b5e90d38f400e23eec1b47a6cc34c6285d2741f0
Light Yagami <abdulsammad200@gmail.com>
2019-11-01 10:55:37 +0500

    Updated keys task

--- tasks/keysdaysleft.js
+++ tasks/keysdaysleft.js
@@ -16,9 +16,7 @@ module.exports = class extends Task {
     const guilds = await Guild.find({}).exec();
 
     guilds.forEach(async (guild) => {
-      if (guild.daysLeft > 0) guild.daysLeft--;
-
-      if (guild.daysLeft === 0) {
+      if (guild.daysLeft === 1) {
         await redisClient.sremAsync('valid_guilds', guild.guildID);
         await redisClient.hdelAsync('guild_tiers', guild.guildID);
         this.client.guilds
@@ -27,6 +25,9 @@ module.exports = class extends Task {
             'Your trial of Animu has expired, Join patreon to continue using Anime: https://patreon.com/Aldovia',
           );
       }
+
+      if (guild.daysLeft > 0) guild.daysLeft--;
+
       await guild.save();
     });
   }
